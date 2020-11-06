9f0ca1f0cd6bb8696c440018f5869f9ab39c6622
Al <al.almaktoum@live.com>
2020-11-07 09:09:48 +1100

    Set up click events on a button wrapper and Headless UI transition worked

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -166,19 +166,17 @@ const Nav = () => {
                     className='rounded-full flex text-sm text-white focus:outline-none focus:shadow-solid transition duration-150 ease-in-out'
                     id='user-menu'
                     aria-haspopup='true'
+                    onClick={() => setProfileOptionsDropdownVisibiity(true)}
+                    onBlur={() =>
+                      setTimeout(
+                        () => setProfileOptionsDropdownVisibiity(false),
+                        50
+                      )
+                    }
                   >
                     <span className='sr-only'>Open profile menu</span>
 
                     <img
-                      onMouseEnter={() =>
-                        setProfileOptionsDropdownVisibiity(true)
-                      }
-                      onBlur={() =>
-                        setTimeout(
-                          () => setProfileOptionsDropdownVisibiity(false),
-                          50
-                        )
-                      }
                       className='rounded-full h-8 w-8'
                       src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
                       alt=''
@@ -204,17 +202,7 @@ const Nav = () => {
                   leaveFrom='transform opacity-100 scale-100'
                   leaveTo='transform opacity-0 scale-95'
                 >
-                  <div
-                    className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg'
-                    onFocus={() => () =>
-                      setProfileOptionsDropdownVisibiity(true)}
-                    onBlur={() =>
-                      setTimeout(
-                        () => setProfileOptionsDropdownVisibiity(false),
-                        300
-                      )
-                    }
-                  >
+                  <div className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg'>
                     <div
                       className='py-1 bg-white rounded-md shadow-xs'
                       role='menu'
